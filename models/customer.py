3d1093b842bf5a7002c5cfbb8d9c63dd18a059bf
YassenHatem <yaseenhatem2@fci.helwan.edu.eg>
2023-07-11 18:23:11 +0300

    Create customer model.

---
+++ models/customer.py
@@ -0,0 +1,10 @@
+"""Customer Model Class."""
+
+
+class Customer:
+    def __init__(self, identifier: int, name: str, email: str, phone_number: str, address: str):
+        self.identifier = identifier
+        self.name = name
+        self.email = email
+        self.phone = phone_number
+        self.address = address
